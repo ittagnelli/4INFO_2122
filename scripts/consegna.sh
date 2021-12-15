62b47d95c6bc5193615608111dc1781991a4374f
Chindris Alexandru <alexandruioan.chindris@istitutoagnelli.it>
2021-12-15 22:01:36 +0100

    Test finale

--- scripts/consegna.sh
+++ scripts/consegna.sh
@@ -3,12 +3,13 @@
 #Created on 14 december 2021.    Last update: 15 december 2021.
 #!/bin/bash
 
+echo "The script is running... "
 allinput="${REPLY}"
 git pull
-git status
 git add .
 echo "Add a commit for your push: "
 read
 git commit -m "${REPLY}"
 git status
 git push
+echo "The script is ended"
