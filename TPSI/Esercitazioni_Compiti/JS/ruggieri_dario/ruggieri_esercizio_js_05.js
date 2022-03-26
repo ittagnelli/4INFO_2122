d289c868645c36c581e2a4e44b0e8fc541e612f5
Ruggieri Dario <dario.ruggieri@istitutoagnelli.it>
2022-03-26 13:00:54 +0100

    Aggiunta del pulsante rimuovi es 05 - JS

--- TPSI/Esercitazioni_Compiti/JS/ruggieri_dario/ruggieri_esercizio_js_05.js
+++ TPSI/Esercitazioni_Compiti/JS/ruggieri_dario/ruggieri_esercizio_js_05.js
@@ -1,28 +1,52 @@
 function add_studente(){
-    console.log("La funzione è stata chiamata");
     let tabella = document.getElementById("tabella");
     let media = document.getElementById("media").value;
-    console.log("media: " + media);
     let nome = document.getElementById("nome").value;
-    console.log("nome: " + nome);
     let tr = document.createElement("tr");
     let tdnome = document.createElement("td");
     let tdmedia = document.createElement("td");
     tabella.append(tr);
     tdnome.innerText = nome;
     tdmedia.innerText = media;
+    tdnome.className = "nome_str";
     tr.append(tdnome);
     tr.append(tdmedia);
 }
 
 function colora(){
     let trs = document.getElementsByTagName("tr");
-    console.log(trs);
-    console.log(trs.length);
     for(let i = 0; i < trs.length; i++){
         if(i % 2  == 1)
             trs[i].style.backgroundColor = "blue";
         else
             trs[i].style.backgroundColor = "red";
     }
+}
+
+function remove_studente(){
+    let nome = document.getElementById("nome").value;
+    let trs = document.getElementsByTagName("tr");
+    let tds = document.getElementsByClassName("nome_str");
+    let flag = 0;
+    let trs_lenght = trs.length;
+    for(let i = trs_lenght-1; i >= 0; i--){
+        if(tds[i].innerText == nome){
+            trs[i].remove();
+            flag = 1;
+        }
+    }
+    if(!flag)
+        alert("Persona non presente nella tabella");
+}
+function start(){
+    const body = document.body;
+    let risultato = prompt("Come ti chiami", "Sostituisci con il tuo nome");
+    let h1 = document.getElementsByTagName("h1");
+    if(risultato && risultato != "Sostituisci con il tuo nome"){
+        h1[0].innerText = "Benvenuto " + risultato;
+    }
+    else
+        alert("L'utente non ha inserito il suo nome");
+
+
 }
