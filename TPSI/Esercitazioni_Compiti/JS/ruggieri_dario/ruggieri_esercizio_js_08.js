82a1ac3d5dac8780b5550982517bfa1588c8d78d
Ruggieri Dario <dario.ruggieri@istitutoagnelli.it>
2022-05-14 14:35:52 +0200

    Miglioramento es 08 - JS

--- TPSI/Esercitazioni_Compiti/JS/ruggieri_dario/ruggieri_esercizio_js_08.js
+++ TPSI/Esercitazioni_Compiti/JS/ruggieri_dario/ruggieri_esercizio_js_08.js
@@ -1,53 +1,56 @@
-function work(){
+let id = 0;
+
+function work() {
     alert("The button is working");
 }
-function rimuovi(da_rimuovere){
-    let tr = da_rimuovere.parentElement.parentElement;
+function rimuovi(da_rimuovere) {
+    let tr = document.getElementById(da_rimuovere);
     tr.remove();
 }
-function salva(){
+function salva() {
     let todo = document.getElementById("ToDo").value;
     let priority = document.getElementById("idpriority").value;
     let db_table = document.getElementById("db_table");
     let svuota_todo = document.getElementById("ToDo");
     let tr = document.createElement("tr");
-    if(todo == ""){
+    if (todo == "") {
         alert("Completare tutti i campi");
     }
-    else{
-        for(let i = 0; i < 4; i++){
-        let td = document.createElement("td");
-        let span = document.createElement("span");
-        let button = document.createElement("button");
-        span.classList.add("material-icons");
-            switch(i){
+    else {
+        for (let i = 0; i < 4; i++) {
+            let td = document.createElement("td");
+            let span = document.createElement("span");
+            let button = document.createElement("button");
+            span.classList.add("material-icons");
+            switch (i) {
                 case 0:
                     td.setAttribute("colspan", 2);
+                    td.setAttribute("id" , "td_" + id);
                     td.innerText = todo;
                     break;
                 case 1:
-                    if(priority == "alta"){
+                    if (priority == "alta") {
                         span.innerText = "north";
                         span.classList.add("red");
                     }
-                    else if(priority == "media"){
+                    else if (priority == "media") {
                         span.innerText = "east";
                         span.classList.add("yellow");
                     }
-                    else if(priority == "bassa"){
+                    else if (priority == "bassa") {
                         span.innerText = "south";
                         span.classList.add("green");
                     }
                     td.append(span);
                     break;
                 case 2:
-                    button.setAttribute("onclick", "completato(this)");
+                    button.setAttribute("onclick", `completato(${id})`);
                     span.innerText = "done_outline";
                     button.append(span);
                     td.append(button);
                     break;
                 case 3:
-                    button.setAttribute("onclick", "rimuovi(this)");
+                    button.setAttribute("onclick", `rimuovi(${id})`);
                     span.classList.remove("material-icons");
                     span.classList.add("material-symbols-outlined");
                     span.innerText = "delete";
@@ -56,16 +59,17 @@ function salva(){
                     break;
                 default:
                     break;
-
+                }  
+                tr.append(td);     
             }
-            tr.append(td);
+            tr.setAttribute("id" , id);
+            console.log(tr);  
+            id++;
             db_table.append(tr);
-        }       
+        }
+        svuota_todo.value = "";
     }
-    svuota_todo.value = "";
-}
-function completato(oggetto){
-    let tr = oggetto.parentElement.parentElement;
-    let td_todo = tr.firstChild;
-    td_todo.classList.toggle("underline")
-}
+    function completato(da_completare) {
+        let td = document.getElementById("td_" + da_completare);    
+        td.classList.toggle("underline");
+    }
